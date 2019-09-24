a842c20351b68cca73b7fc037c001a4de534aaad
Unknown <paola.qdr@gmail.com>
2019-09-24 08:21:55 -0500

    Commit defining Models

--- apps/usuarios/models.py
+++ apps/usuarios/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+'''
+** clave empleado
+Usuarios
+    * user
+        - id
+        - email
+        - password
+        - nombre
+        - apellido
+        - fecha_inicio
+        - area
+        - sueldo
+        - is_super_user
+        - is_area_admin
+
+    * cliente
+        - claveCliente
+        - nombre
+        - RFC
+        - direccion
+        - correo
+        - telefono
+    
+'''
+
+
 # Create your models here.
